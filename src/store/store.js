e41b1fcf7c7ce5cfbaaadc20897b80a6155fb382
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-01-22 20:10:26 +0200

    store.js mvp setup'
    fda

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,34 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-
+import { configureStore } from '@reduxjs/toolkit';
+// import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { contactsApi } from './contactsApi';
 import { contactsReducer } from './contacts/slice';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-});
-
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware => getDefaultMiddleware(),
+  reducer: {
+    contacts: contactsReducer,
+    [contactsApi.reducerPath]: contactsApi.reducer,
+  },
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware(),
+    contactsApi.middleware,
+  ],
   devTools: process.env.NODE_ENV === 'development',
 });
+
+// setupListeners(store.dispatch);
+
+// hw6
+
+// import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+// import { contactsReducer } from './contacts/slice';
+
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer,
+// });
+
+// export const store = configureStore({
+//   reducer: rootReducer,
+//   middleware: getDefaultMiddleware => getDefaultMiddleware(),
+//   devTools: process.env.NODE_ENV === 'development',
+// });
