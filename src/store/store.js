acdf38bf8e1fa6fd7e2575883d262aad2e7d1c10
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-01-22 19:07:58 +0200

    change discription, title and favicon

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,5 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts/slice';
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
@@ -15,11 +7,6 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  middleware: getDefaultMiddleware => getDefaultMiddleware(),
   devTools: process.env.NODE_ENV === 'development',
 });
