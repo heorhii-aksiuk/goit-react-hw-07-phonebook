b3058d9f4cea2816f54fcdb8f6a68ef8bf6ed204
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-01-23 17:13:27 +0200

    CRLF test

--- src/store/contactsApi.js
+++ src/store/contactsApi.js
@@ -3,11 +3,11 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const contactsApi = createApi({
   reducerPath: 'contactsApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://61ec28a1f3011500174d20d4.mockapi.io/api/',
+    baseUrl: 'https://61ec28a1f3011500174d20d4.mockapi.io/api',
   }),
   endpoints: builder => ({
     getContact: builder.query({
-      query: id => `contacts/${id}`,
+      query: id => `/contacts/${id}`,
     }),
   }),
 });
